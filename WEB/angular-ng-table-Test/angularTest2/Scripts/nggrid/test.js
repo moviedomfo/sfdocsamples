2710230bb3ba9efafb0e792c37090d31356066a4
Marcelo Fabián <marcelo.oviedo@gmail.com>
2016-04-03 13:17:03 -0300

    s

---
+++ WEB/angular-ng-table-Test/angularTest2/Scripts/nggrid/test.js
@@ -0,0 +1,91 @@
+﻿
+var app;
+(function () {
+    app = angular.module("myApp", ['ngGrid']);
+})();
+app.service('EmployeeService', function ($http) {
+
+    this.getAllEmployee = function () {
+        return $http.get(httpHost + "/api/person/GetAll");
+    }
+
+});
+
+
+
+
+app.controller('EmployeeController', function ($scope, EmployeeService) {
+
+    GetAllRecords();
+    function GetAllRecords() {
+        var promiseGet = EmployeeService.getAllEmployee();
+        promiseGet.then(function (pl) { $scope.Employees = pl.data, $scope.myData = pl.data },
+              function (errorPl) {
+                  $log.error('Some Error in Getting Records.', errorPl);
+              });
+    }
+    $scope.myData = [];
+    $scope.gridOptions = {
+
+        data: 'myData',
+        pagingOptions: $scope.pagingOptions,
+        enablePinning: true,
+        enablePaging: true,
+        showFooter: true,
+        enableColumnResize: true,
+        enableCellSelection: true,
+        columnDefs: [
+                {
+                    field: "Name",
+                    width: 180,
+                    pinned: true,
+                    enableCellEdit: true
+                },
+                {
+                    field: "Person Id",
+                    width: 200,
+                    enableCellEdit: true
+                },
+                {
+                    field: "Lastname",
+                    width: 100,
+                    enableCellEdit: true
+                },
+                {
+                    field: "docNumber",
+                    width: 120,
+                    enableCellEdit: true,
+                    cellTemplate: basicCellTemplate
+                },
+                {
+                    field: "Action",
+                    width: 200,
+                    enableCellEdit: false,
+                    cellTemplate: '<button id="editBtn" type="button" class="btn btn-xs btn-info"  ng-click="updateCell()" >Edit </button>'
+                }]
+
+    };
+
+    $scope.selectedCell;
+    $scope.selectedRow;
+    $scope.selectedColumn;
+
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: true
+    };
+
+    $scope.gridOptions.sortInfo = {
+        fields: ['Company', 'Price'],
+        directions: ['asc'],
+        columns: [0, 1]
+    };
+    $scope.totalServerItems = 0;
+
+    $scope.pagingOptions = {
+        pageSizes: [5, 10, 20],
+        pageSize: 5,
+        currentPage: 1
+    };
+    var basicCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()" ng-click="editCell(row.entity, row.getProperty(col.field), col.field)"><span class="ui-disableSelection hover">{{row.getProperty(col.field)}}</span></div>';
+});
