74d5e0d4a4696b6d3d7ac640531794420aafd169
Marcelo Fabián <marcelo.oviedo@gmail.com>
2016-04-03 13:20:01 -0300

    enter

--- WEB/angular-ng-table-Test/angularTest2/Scripts/nggrid/test.js
+++ WEB/angular-ng-table-Test/angularTest2/Scripts/nggrid/test.js
@@ -24,6 +24,7 @@ app.controller('EmployeeController', function ($scope, EmployeeService) {
                   $log.error('Some Error in Getting Records.', errorPl);
               });
     }
+
     $scope.myData = [];
     $scope.gridOptions = {
 
