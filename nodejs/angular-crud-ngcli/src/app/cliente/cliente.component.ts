3739bb38004cbe135309ca5ccd6244c2a86e3e90
Marcelo Fabián <marcelo.oviedo@gmail.com>
2017-05-28 13:53:19 -0300

    limpieza y  dropdown selected

--- nodejs/angular-crud-ngcli/src/app/cliente/cliente.component.ts
+++ nodejs/angular-crud-ngcli/src/app/cliente/cliente.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 //  import { ModalDialogComponent } from '../modal-dialog/modal-dialog.component';
 //  import { DialogService } from "ng2-bootstrap-modal";
-    
-    
+
+
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
@@ -10,31 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
+  private selectedPais:String ='';
+  public paises: Array<String> = ["Afghanistan",
+    "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+    "Bangladesh", "Cote d'Ivoire", "Croatia", "Cuba"];
+
+
   //constructor(private dialogService:DialogService) { }
-constructor(){}
+  constructor() {
+    //let paises: Array<number> = [1, 2, 3];
+
+  }
   ngOnInit() {
   }
 
-  createClient(event){
-    alert('Crear cliente');
+  createClient(event) {
+    var str: String = 'Pais seleccionado ' + this.selectedPais;
+  alert (str);
+   var paicesFiltrados =  this.paises.filter(p=>p.startsWith('Ar') );
+    
+    
+    paicesFiltrados.forEach(element => {
+        alert(element);
+    });
+    //alert(str);
+
   }
-// showConfirm() {
-//             let disposable = this.dialogService.addDialog(ModalDialogComponent, {
-//                 title:'Confirm title', 
-//                 message:'Confirm message'})
-//                 .subscribe((isConfirmed)=>{
-//                     //We get dialog result
-//                     if(isConfirmed) {
-//                         alert('accepted');
-//                     }
-//                     else {
-//                         alert('declined');
-//                     }
-//                 });
-//             //We can close dialog calling disposable.unsubscribe();
-//             //If dialog was not closed manually close it by timeout
-//             setTimeout(()=>{
-//                 disposable.unsubscribe();
-//             },10000);
-//         }
+
+onPaisSelection(event){
+    alert( this.selectedPais);
+
+  }
+  // showConfirm() {
+  //             let disposable = this.dialogService.addDialog(ModalDialogComponent, {
+  //                 title:'Confirm title', 
+  //                 message:'Confirm message'})
+  //                 .subscribe((isConfirmed)=>{
+  //                     //We get dialog result
+  //                     if(isConfirmed) {
+  //                         alert('accepted');
+  //                     }
+  //                     else {
+  //                         alert('declined');
+  //                     }
+  //                 });
+  //             //We can close dialog calling disposable.unsubscribe();
+  //             //If dialog was not closed manually close it by timeout
+  //             setTimeout(()=>{
+  //                 disposable.unsubscribe();
+  //             },10000);
+  //         }
 }
---
+++ nodejs/rxjs-socket.io-master/client/src/app/client.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'client-app',
+  templateUrl: './client.component.html'
+})
+export class ClientAppComponent {
+}
