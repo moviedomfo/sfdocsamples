2724bc496fb60aab462f1209c56f8f9d1a0e1d07
Marcelo Fabián <marcelo.oviedo@gmail.com>
2017-06-28 19:41:07 -0300

    patients

--- nodejs/angular-crud-ngcli/src/app/service/patients.service.ts
+++ nodejs/angular-crud-ngcli/src/app/service/patients.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IPatient, PatientBE } from '../model/patients.model';
-import {Http,Response} from '@angular/http';
-import {  PersonsBE } from '../model/persons.model';
+import { Http, Response } from '@angular/http';
+import { PersonsBE } from '../model/persons.model';
 //permmite cambiar la variable obsevada
 import { Subject } from 'rxjs/Subject';
 //permite observar
@@ -9,27 +9,43 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PatientsService {
-  private PatientsList: IPatient[] = [];
-  private PatientsList$: Subject<IPatient[]> = new Subject<IPatient[]>();
-  
-  
-  private cars: any[] = [
-    'Ford', 'Chevrolet', 'Buick'
-  ];
-
-  constructor(private http : Http ) { } 
+  private PatientList: PatientBE[] = [];
+  private PatientList$: Subject<IPatient[]> = new Subject<IPatient[]>();
 
 
+  private patient: PatientBE;
 
-  createPatients(Patients: PatientBE) {
+  constructor(private http: Http) {
+
+    this.PatientList = [
+      {
+        PatientId: 100,
+
+        IdPersona: 1,
+        Apellido: "Oviedo"
+      },
+
+      {
+        PatientId: 110,
+        IdPersona: 22,
+        Apellido: "Hendryxo"
+      },
+
+    ];
+
+  }
+
+
+
+  createPatients(patient: PatientBE) {
 
     //Clona Patients por parametro
-    var movClone: IPatient = Object.assign({}, Patients);
-    this.PatientsList.push(movClone);
+    var patientClone: IPatient = Object.assign({}, patient);
+    this.PatientList.push(patientClone);
     //esto es lo siguiente q envio o notifico. lo que le envio es algo q coincida con la declaracion
     //Subject<IPatients[]>
     //Emito un evento.
-    this.PatientsList$.next(this.PatientsList);
+    this.PatientList$.next(this.PatientList);
     // mov.id=0;
     // mov.fecha= new Date(Date.now());
     // mov.tipoId=11;
@@ -43,16 +59,23 @@ export class PatientsService {
 
 
   }
+  getPatientsListHttp$(): Observable<PatientBE[]> {
+
+    //map retorna el mapeo de un json que viene del servicio que tiene la misma estructura que  PatientBE
+    return this.http.get('../api/patient').map(function (res: Response) {
+      return res.json();
+    });
+  }
 
   getPatientsList$(): Observable<PatientBE[]> {
 
-    return this.PatientsList$.asObservable();
+    return this.PatientList$.asObservable();
   }
 
   getPatients(movId: number): IPatient {
-    let mov: IPatient;
-    mov = this.PatientsList.filter(p => p.PatientId === movId)[0];
-    return mov;
+    let patient: IPatient;
+    patient = this.PatientList.filter(p => p.PatientId === movId)[0];
+    return patient;
   }
   myData() {
     return 'This is my data, man!';
