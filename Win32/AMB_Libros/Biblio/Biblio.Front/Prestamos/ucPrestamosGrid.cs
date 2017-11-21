65a6d58f00ee70a2ad14817f3a173dcbb2dece99
Marcelo F Oviedo <marcelo.oviedo@gmail.com>
2017-11-21 09:17:42 -0300

    Merge branch 'master' of https://github.com/moviedomfo/sfdocsamples
    Conflicts:
    	Win32/AMB_Libros/Biblio/Biblio.DAC/DAC/PrestamosDAC.cs
    	Win32/AMB_Libros/Biblio/Biblio.Front/Biblio.Front.csproj
    	Win32/AMB_Libros/Biblio/Biblio.Front/Libros/frmLibrosAlta.Designer.cs
    	Win32/AMB_Libros/Biblio/Biblio.Front/Prestamos/frmDevolucion.Designer.cs
    	Win32/AMB_Libros/Biblio/Biblio.Front/Properties/Resources.Designer.cs
    	Win32/AMB_Libros/Biblio/Biblio.Front/Properties/Resources.resx
    	Win32/AMB_Libros/Biblio/Biblio.Front/Properties/Resources.resx.bak
    	Win32/AMB_Libros/Biblio/Biblio.Front/frmPrestamo.Designer.cs
    	Win32/AMB_Libros/Biblio/Biblio.Front/frmPrestamo.cs
    	Win32/AMB_Libros/Biblio/Biblio.Front/socios/frmSocioGrid.cs

---
+++ Win32/AMB_Libros/Biblio/Biblio.Front/Prestamos/ucPrestamosGrid.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Biblio.Common.BE;
+using Biblio.DAC;
+
+namespace Biblio.Front
+{
+    [ToolboxItem(true)]
+    [DefaultEvent("OnClickEvent")]
+    public partial class ucPrestamosGrid : UserControl
+    {
+        public event EventHandler OnClickEvent;
+        public PrestamosView currentPrestamosView;
+        public PrestamosViewList prestamosViewList;
+        public ucPrestamosGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            currentPrestamosView = (PrestamosView)this.prestamosViewListBindingSource.Current;
+            if (OnClickEvent != null)
+            {
+                OnClickEvent(this, new EventArgs());
+            }
+        }
+        public void Search()
+        {
+            prestamosViewList = PrestamosDAC.Search();
+            Filter(false);
+        }
+        public void Filter(Boolean incluirDevueltos)
+        {
+            PrestamosViewList prestamosViewListAux = new PrestamosViewList();
+            if(prestamosViewList==null)
+            { return; }
+            if (incluirDevueltos)
+            {
+
+                prestamosViewListAux = this.prestamosViewList;
+               
+            }
+            else
+            {
+                var filterList = prestamosViewList.Where(p => p.Estado != "Devuelto");
+                prestamosViewListAux.AddRange(filterList.ToList());
+            }
+            
+
+            this.prestamosViewListBindingSource.DataSource = prestamosViewListAux;
+        }
+    }
+}
+
