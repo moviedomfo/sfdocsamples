fb4ac6ff2f46329f8449046a8277b6a1d041e192
moviedomfo <marcelo.oviedo@gmail.com>
2018-03-28 17:39:56 -0300

    identity

---
+++ WEB/Security/customIdentityClaims/LocalAccountsApp/app_identity/IdentityConfig.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using LocalAccountsApp.Models;
+using System;
+
+namespace LocalAccountsApp.app_identity
+{
+    // Configure the application user manager used in this application. 
+    //UserManager is defined in ASP.NET Identity and is used by the application.
+
+    public class ApplicationUserManager : UserManager<ApplicationUser, Guid>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser, Guid> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            //var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser,
+                                                ApplicationRole,
+                                                Guid,
+                                                ApplicationUserLogin,
+                                                ApplicationUserRole,
+                                                ApplicationUserClaim>
+                                                (context.Get<ApplicationDbContext>()));
+
+            // Configure validation logic for usernames
+            manager.UserValidator = new CustomUserValidator(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser,Guid>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+}
