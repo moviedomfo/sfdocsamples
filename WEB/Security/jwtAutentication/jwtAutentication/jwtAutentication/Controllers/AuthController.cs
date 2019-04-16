32a3cd2a38b04a28a6a157148e0f7c06b0c63f0a
moviedomfo2 <marcelo.oviedo@gmail.com>
2019-04-16 18:07:41 -0300

    JWT SAMPLE

---
+++ WEB/Security/jwtAutentication/jwtAutentication/jwtAutentication/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading;
+using jwtAuthentication.Models;
+
+namespace jwtAuthentication.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/auth")]
+    public class AuthController : ApiController
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //TODO: Validate credentials Correctly, this code is only for demo !!
+            bool isCredentialValid = (login.Password == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ WEB/Security/jwtAutentication/jwtAutentication/jwtAutentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jwtAuthentication.WebApiApplication" Language="C#" %>
