aae4efe072f4b6936aaf711f538d7200bcd04e4d
Marcelo <Oviedo>
2021-01-02 18:00:22 -0300

    nlog

--- aspnet-core/template/fwk.template.api/Program.cs
+++ aspnet-core/template/fwk.template.api/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using EnvironmentName = Microsoft.AspNetCore.Hosting.EnvironmentName;
 
 namespace pelsoft.api
 {
@@ -13,14 +16,40 @@ namespace pelsoft.api
     {
         public static void Main(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var isProd = environment == EnvironmentName.Production;
+            var logger = NLogBuilder.ConfigureNLog(isProd ? "nlog.config" : "nlog.debug.config").GetCurrentClassLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                    .ConfigureLogging((context, logging) =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                        //logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                        //logging.AddDebug();
+                        //logging.AddConsole();
+                        logging.AddNLog();
+                    })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
     }
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>();
+    //            });
+    //}
 }
